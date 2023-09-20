2c666662f03f688ab8bda8afe1c5e0decf86147d
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-09-20 00:23:44 -0500

    Used imports

--- Workshop-1/closest-minimum-height.cpp
+++ Workshop-1/closest-minimum-height.cpp
@@ -1,5 +1,10 @@
 // Made by @DaniDiazTech
+// Need to copy and past DS implementations to send to online judge
+
 #include <iostream>
+// #include "../DS-implementations/Stack/StackArray.h"
+#include "../DS-implementations/Stack/StackLinkedList.h"
+#include "../DS-implementations/Miscellaneous/Pair.h"
 
 // Implementation of stack (linear data structure)
 
@@ -8,71 +13,6 @@
 // pop: O(1)
 // empty: O(1)
 // size: O(1)
-
-template<typename T> struct Stack{
-  int _top, capacity;
-  T *data;
-
-  // Constructor
-  Stack(){
-    _top = 0;
-    // Initial capacity: 16
-    capacity = 16;
-    data = new T[capacity];
-  }
-
-  bool empty(){
-    return _top == 0;
-  }
-
-  int size(){
-    return _top;
-  }
-
-  T top(){
-    if (!empty())
-      return data[_top - 1];
-    else
-      throw "Can't access element of empty Stack";
-  }
-
-  T pop(){
-    if (empty())
-      throw "Can't pop empty stack";
-    return data[--_top];
-  }
-
-  void push(T element){
-    if (capacity == size()){
-      // Resize array
-      capacity *= 2;
-      T *new_data = new T[capacity];
-
-      // Copy elements from previous array
-      for (int i = 0; i < size(); i++){
-        new_data[i] = data[i]; 
-      }
-
-      std::swap(data, new_data);
-      delete [] new_data;
-    }
-
-    data[_top++] = element;
-  }
-};
-
-// Auxiliary struct
-// Keeps two elements of X and Y type
-template<typename X, typename Y> struct Pair{
-  X first;
-  Y second;
-  Pair(X f, Y s){
-    first = f;
-    second = s;
-  }
-  Pair(){
-  }
-};
 
 // Overall complexity O(n)
 // Each element is inserted at most once, and popped once in the stack 
